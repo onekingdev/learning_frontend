ea8b975d7120499224476a103a8d7700c987bb36
onekingdev <onekingdev@gmail.com>
2021-05-18 14:32:35 -0500

    My progress organism

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import {LogIn} from './views/pages/Login/Login';
+import React from 'react';
+import {MyProgress} from './views/organisms/MyProgress';
+import {
+  Headline1,
+  Headline2,
+  Headline3,
+  Headline4,
+  Headline5,
+  Headline6,
+} from './views/atoms/Headline/Headline';
+import {Header} from './views/organisms/Header';
 
 function App() {
   return (
     <div className="App">
-      <LogIn />
+      <Header />
+      <MyProgress />
+      <Headline1 body={'H1 Head'} isDark={true} />
+      <Headline2 body={'H1 Head'} isDark={true} />
+      <Headline3 body={'H1 Head'} isDark={true} />
+      <Headline4 body={'H1 Head'} isDark={true} />
+      <Headline5 body={'H1 Head'} isDark={true} />
+      <Headline6 body={'H1 Head'} isDark={true} />
     </div>
   );
 }
---
+++ src/views/molecules/Energy/Energy.tsx
@@ -0,0 +1,29 @@
+import {FC} from 'react';
+import {Icon} from '../../atoms/Icon/Icon';
+import lightning from '../../assets/lightning.svg';
+import {Battery} from '../../atoms/Battery/Battery';
+import {IconSize} from '../../atoms/Icon/Size';
+import styled from 'styled-components';
+import {BatteryButton} from '../../atoms/Battery/BatteryButton';
+
+type EnergyProps = {};
+
+const EnergyStyle = styled.div`
+  width: 320px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Energy: FC<EnergyProps> = () => {
+  return (
+    <>
+      <EnergyStyle>
+        <Icon image={lightning} size={IconSize.small} />
+        <Battery charge={4} />
+        <BatteryButton value="GET!" onClick={() => console.log('Hella yes')} />
+      </EnergyStyle>
+    </>
+  );
+};
