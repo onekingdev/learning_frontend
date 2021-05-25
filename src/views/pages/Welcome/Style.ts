20827c8c77464c9fc68fb29bafe6dd3c00cb9530
onekingdev <onekingdev@gmail.com>
2021-05-25 17:33:43 -0500

    welcome page responsive, with dictionary

--- src/views/pages/Welcome/Style.ts
+++ src/views/pages/Welcome/Style.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import background from '../../assets/colored-shapes-bg.svg';
-// import {BasicColor} from '../../Color';
 import {ScreenSize} from '../../screenSize';
 
 export const Wrapper = styled.div`
@@ -12,18 +11,76 @@ export const Wrapper = styled.div`
 
 export const Logo = styled.img`
   width: 15rem;
-  position: absolute;
-  top: 1.2rem;
-  left: 1rem;
+  padding-left: 1rem;
+  padding-top: 1.2rem;
+  @media (min-width: ${ScreenSize.tablet}) {
+    width: 35rem;
+    padding-top: 6rem;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  @media (min-width: ${ScreenSize.desktop}) {
+    width: 15rem;
+  }
+`;
+
+export const Illustration = styled.img`
+  width: 95vw;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  @media (min-width: ${ScreenSize.desktop}) {
+    width: 32rem;
+  }
+`;
+
+export const Body = styled.div`
+  margin-top: 1.2rem;
+  padding: 2rem;
+  text-align: center;
+  @media (min-width: ${ScreenSize.tablet}) {
+    margin-top: 3rem;
+    margin-bottom: 3rem;
+  }
+  @media (min-width: ${ScreenSize.desktop}) {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    margin-top: 1rem;
+  }
+`;
+
+export const Actions = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 1rem;
+  width: 215px;
+  margin-top: 3rem;
+  margin-left: auto;
+  margin-right: auto;
+  @media (min-width: ${ScreenSize.tablet}) {
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 114px;
+    width: 534px;
+    margin-top: 5rem;
+  }
+  @media (min-width: ${ScreenSize.tablet}) {
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 114px;
+    width: 534px;
+    margin-top: 3rem;
+  }
 `;
 
 export const Legal = styled.div`
-  display: none;
-  @media (min-width: ${ScreenSize.desktop}) {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    position: absolute;
-    bottom: 2rem;
-    left: calc(50vw + 3rem);
-  }
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  position: absolute;
+  bottom: 2rem;
+  width: 85vw;
+  margin-left: 7.5vw;
+`;
+
+export const Description = styled.div`
+  margin-top: 1.2rem;
 `;
--- src/views/pages/Welcome/Welcome.tsx
+++ src/views/pages/Welcome/Welcome.tsx
@@ -5,82 +5,53 @@ import {Subheader} from '../../atoms/Text/Subheader';
 import welcome from '../../assets/welcome-page.svg';
 import {Button} from '../../molecules/Button';
 import {ButtonColor} from '../../Color';
-import styled from 'styled-components';
-import background from '../../assets/colored-shapes-bg.svg';
 import {Icon} from '../../atoms/Text/Icon';
+import {dictionary} from '../Welcome/dictionary';
+
+import {
+  Wrapper,
+  Logo,
+  Body,
+  Description,
+  Illustration,
+  Actions,
+  Legal,
+} from './Style';
 
 export const Welcome: FC = () => {
+  const language = 'en';
+
   return (
     <Wrapper>
       <Logo src={logo} alt="Learn with Socrates logo" />
       <Body>
-        <Header isDark={true}>Practice, play, grow</Header>
+        <Header isDark={true}>{dictionary[language].practice}</Header>
         <Description>
           <Subheader isDark={true}>
-            Helping students to dominate and retain educational content through
-            personalized automation
+            {dictionary[language].description}
           </Subheader>
         </Description>
       </Body>
       <Illustration src={welcome} alt="" />
       <Actions>
-        <Button value={'log in'} color={ButtonColor.login} />
-        <Button value={'join'} color={ButtonColor.join} darkText={true} />
+        <Button value={dictionary[language].login} color={ButtonColor.login} />
+        <Button
+          value={dictionary[language].join}
+          color={ButtonColor.join}
+          darkText={true}
+        />
       </Actions>
       <Legal>
-        <Icon isDark={true}>about</Icon>
-        <Icon isDark={true}>privacy</Icon>
-        <Icon isDark={true}>children_privacy</Icon>
+        <Icon style={{textAlign: 'center'}} isDark={true}>
+          {dictionary[language].about}
+        </Icon>
+        <Icon style={{textAlign: 'center'}} isDark={true}>
+          {dictionary[language].privacy}
+        </Icon>
+        <Icon style={{textAlign: 'center'}} isDark={true}>
+          {dictionary[language].children_privacy}
+        </Icon>
       </Legal>
     </Wrapper>
   );
 };
-
-const Wrapper = styled.div`
-  background-image: url(${background});
-  background-repeat: no-repeat;
-  background-size: cover;
-  height: 100vh;
-`;
-
-const Logo = styled.img`
-  width: 15rem;
-  padding-left: 1rem;
-  padding-top: 1.2rem;
-`;
-
-const Illustration = styled.img`
-  width: 95vw;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const Body = styled.div`
-  margin-top: 1.4rem;
-  padding: 2rem;
-  text-align: center;
-`;
-
-const Actions = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-gap: 1rem;
-  width: 215px;
-  margin-top: 3rem;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const Legal = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  position: absolute;
-  bottom: 2rem;
-  width: 85vw;
-  margin-left: 7.5vw;
-`;
-
-const Description = styled.div`
-  margin-top: 1.2rem;
-`;
