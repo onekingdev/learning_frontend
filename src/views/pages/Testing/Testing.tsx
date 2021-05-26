de4f3b6c7dccae0a804b94101baa0fa4750d9e6a
onekingdev <onekingdev@gmail.com>
2021-05-26 15:54:09 -0500

    Testing page

--- src/views/pages/Testing/Testing.tsx
+++ src/views/pages/Testing/Testing.tsx
@@ -1,5 +1,14 @@
 import {FC} from 'react';
+import {LessonProgress} from '../../molecules/LessonProgress/LessonProgress';
 
 export const Testing: FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <LessonProgress
+        topic={'Math'}
+        currentQuestion={3}
+        totalQuestions={10}
+      ></LessonProgress>
+    </div>
+  );
 };
