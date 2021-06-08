398aa644c8aca6ce9a5f672ccc3faa38fdfb92ba
onekingdev <onekingdev@gmail.com>
2021-06-08 13:32:57 -0500

    Fix warnings

--- src/views/pages/Avatar/Style.ts
+++ src/views/pages/Avatar/Style.ts
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
 import background from '../../assets/colored-shapes-bg.svg';
-import {BasicColor} from '../../Color';
-import {ScreenSize} from '../../screenSize';
 
 export const AvatarContainer = styled.div`
   background-image: url(${background});
   background-repeat: no-repeat;
   background-size: cover;
-  height: calc(100vh - 57px);
+  height: 100vh;
 `;
