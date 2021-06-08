e063b119592cc6bb83c9d3ceb49eacf5ebe46212
onekingdev <onekingdev@gmail.com>
2021-06-08 13:05:17 -0500

    Create Avatar molecules

---
+++ src/views/molecules/Avatar/AvatarFavorites.tsx
@@ -0,0 +1,58 @@
+import {FC} from 'react';
+import styled from 'styled-components';
+import {Title} from '../../atoms/Text/Title';
+import {Icon} from '../../atoms/Icon/Icon';
+import {BasicColor} from '../../Color';
+import {ScreenSize} from '../../screenSize';
+
+export const AvatarFavorites: FC = () => {
+  const favorites = [];
+  for (let i = 0; i < 4; i++) {
+    favorites.push({image: 'https://via.placeholder.com/75'});
+  }
+
+  return (
+    <>
+      <FavoritesTitle>
+        <Title isDark={true}>Your Favorites</Title>
+      </FavoritesTitle>
+      <FavoritesCloset>
+        {favorites.map(favorite => (
+          <FavoritesDrawer>
+            <Icon image={favorite.image} round={true}></Icon>
+          </FavoritesDrawer>
+        ))}
+      </FavoritesCloset>
+    </>
+  );
+};
+
+const FavoritesTitle = styled.div`
+  display: grid;
+  background-color: ${BasicColor.yellow};
+  height: 37px;
+  text-align: center;
+  align-content: center;
+  text-transform: uppercase;
+  @media screen and (min-width: ${ScreenSize.phone}) {
+    visibility: hidden;
+  }
+`;
+
+const FavoritesDrawer = styled.div`
+  display: grid;
+  background-color: ${BasicColor.brown};
+  height: 85px;
+  align-content: center;
+  justify-content: center;
+`;
+
+const FavoritesCloset = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 7px;
+  height: 85px;
+  align-content: center;
+  justify-content: center;
+  background-color: ${BasicColor.darkBrown};
+`;
---
+++ src/views/molecules/Avatar/AvatarSelector.tsx
@@ -0,0 +1,43 @@
+import {FC} from 'react';
+import styled from 'styled-components';
+import {Icon} from '../../atoms/Icon/Icon';
+import {BasicColor} from '../../Color';
+import {ScreenSize} from '../../screenSize';
+import myAvatar from '../../assets/avatars/girl-11.svg';
+import arrowLeft from '../../assets/arrows/arrowLeft.svg';
+import arrowRight from '../../assets/arrows/arrowRight.svg';
+
+export const AvatarSelector: FC = () => {
+  return (
+    <>
+      <SelectorGrid>
+        <LeftArrow src={arrowLeft}></LeftArrow>
+        <CurrentAvatar src={myAvatar}></CurrentAvatar>
+        <RightArrow src={arrowRight}></RightArrow>
+      </SelectorGrid>
+    </>
+  );
+};
+
+const SelectorGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-rows: auto;
+  align-content: center;
+  justify-content: center;
+`;
+
+const CurrentAvatar = styled.img`
+  width: 200px;
+  margin: auto;
+`;
+
+const LeftArrow = styled.img`
+  width: 37px;
+  margin: auto;
+`;
+
+const RightArrow = styled.img`
+  width: 37px;
+  margin: auto;
+`;
