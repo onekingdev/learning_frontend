7ebb9d035b996f371b05d1681e1625dcca6e3aa5
onekingdev <onekingdev@gmail.com>
2021-06-08 13:04:35 -0500

    Create Avatar page

--- src/views/pages/Avatar/Avatar.tsx
+++ src/views/pages/Avatar/Avatar.tsx
@@ -1,2 +1,17 @@
 import {FC} from 'react';
-import {Title} from '../../atoms/Text/Title';
+import {AvatarFavorites} from '../../molecules/Avatar/AvatarFavorites';
+import {AvatarSelector} from '../../molecules/Avatar/AvatarSelector';
+import {AvatarContainer} from './Style';
+import {MobileMenu} from '../../organisms/Menu/MobileMenu';
+
+export const Avatar: FC = () => {
+  return (
+    <>
+      <AvatarContainer>
+        <AvatarFavorites />
+        <AvatarSelector />
+      </AvatarContainer>
+      <MobileMenu />
+    </>
+  );
+};
