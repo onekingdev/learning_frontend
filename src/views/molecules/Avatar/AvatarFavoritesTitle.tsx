3528eeabaada14b0abbb42132d564ed35686df63
onekingdev <onekingdev@gmail.com>
2021-06-09 17:34:14 -0500

    Responsivenes

--- src/views/molecules/Avatar/AvatarFavoritesTitle.tsx
+++ src/views/molecules/Avatar/AvatarFavoritesTitle.tsx
@@ -20,6 +20,7 @@ const FavoritesTitle = styled.div`
   align-content: center;
   text-transform: uppercase;
   @media screen and (min-width: ${ScreenSize.phone}) {
-    visibility: hidden;
+    width: auto;
+    grid-row: 1 / 2;
   }
 `;
