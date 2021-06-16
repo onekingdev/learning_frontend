e2ecf6e3a4d8bbd71b5346f27bdda2e93964de9e
onekingdev <onekingdev@gmail.com>
2021-06-16 17:45:50 -0500

    Subtopic card

--- src/views/pages/TopicsMenu/TopicsMenu.tsx
+++ src/views/pages/TopicsMenu/TopicsMenu.tsx
@@ -2,6 +2,7 @@ import {FC} from 'react';
 import {Wrapper, TopicsMenuStyles} from './Style';
 import {StudentMenu} from '../../templates/StudentMenu';
 import {TopicPresentation} from '../../molecules/TopicPresentation';
+import {SubTopicCard} from '../../molecules/SubTopicCard';
 
 export const TopicsMenu: FC = () => {
   return (
@@ -10,6 +11,7 @@ export const TopicsMenu: FC = () => {
         <StudentMenu>
           <TopicsMenuStyles>
             <TopicPresentation title={'MATH'} />
+            <SubTopicCard />
           </TopicsMenuStyles>
         </StudentMenu>
       </Wrapper>
