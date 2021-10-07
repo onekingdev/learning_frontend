e99c86c67068d8037002cad71c2440817303d6b6
onekingdev <onekingdev@gmail.com>
2021-10-07 17:01:09 -0500

    fragments added

--- src/api/fragments.ts
+++ src/api/fragments.ts
@@ -34,6 +34,7 @@ export const LEVEL_FRAGMENT = gql`
 `;
 
 export const ACHIEVEMENT_FRAGMENT = gql`
+  ${LEVEL_FRAGMENT}
   fragment achievementData on AchievementSchema {
     id
     isActive
@@ -48,7 +49,9 @@ export const ACHIEVEMENT_FRAGMENT = gql`
   }
 `;
 
-export const STUDENT_DATA = gql`
+export const STUDENT_FRAGMENT = gql`
+  ${LEVEL_FRAGMENT}, 
+  ${AVATAR_FRAGMENT}
   fragment studentData on StudentSchema {
     id
     isActive
@@ -60,5 +63,105 @@ export const STUDENT_DATA = gql`
     avatar {
       ...avatarData
     }
+  }
+`;
+
+export const ANSWER_FRAGMENT = gql`
+  fragment answerData on AnswerOptionSchema {
+    id
+    identifier
+    isCorrect
+    answerText
+    explanation
+    image
+    audioFile
+    video
+  }
+`;
+
+export const QUESTION_FRAGMENT = gql`
+  ${ANSWER_FRAGMENT}
+  fragment questionData on QuestionSchema {
+    id
+    isActive
+    questionText
+    answeroptionSet {
+      ...answerData
+    }
+  }
+`;
+
+export const BLOCK_PRESENTATION_FRAGMENT = gql`
+  fragment blockPresentationData on BlockPresentationSchema {
+    id
+    isActive
+    hits
+    errors
+    total
+    points
+  }
+`;
+
+export const BLOCK_FRAGMENT = gql`
+  ${QUESTION_FRAGMENT},
+  ${BLOCK_PRESENTATION_FRAGMENT}
+  fragment blockData on BlockSchema{
+	  id
+    isActive
+    modality
+    questions {
+      ...questionData
+    }
+    modality
+    blockpresentationSet{
+      ...blockPresentationData
+    }
+    blockconfigurationSet{
+      id
+      key {
+        id
+        isActive
+        name
+      }
+      value
+    }
+}`;
+
+export const STUDENT_PLAN_FRAGMENT = gql`
+  fragment studentPlanData on StudentPlanSchema {
+    id
+    isActive
+    name
+    slug
+    totalCredits
+    validityDate
+  }
+`;
+
+export const AUDIENCE_FRAGMENT = gql`
+  ${STUDENT_PLAN_FRAGMENT}
+  fragment audienceData on AudienceSchema {
+    id
+    isActive
+    slug
+    name
+    studentplanSet {
+      ...studentPlanData
+    }
+  }
+`;
+
+export const AREAS_OF_KNOWLEDGE_FRAGMENT = gql`
+  ${AUDIENCE_FRAGMENT}
+  fragment areaOfKnowledgeData on AreaOfKnowledgeSchema {
+    id
+    identifier
+    hexColor
+    slug
+    image
+    audience {
+      ...audienceData
+    }
+    name
   }
 `;
