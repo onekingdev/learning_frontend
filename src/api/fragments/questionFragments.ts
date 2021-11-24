a7764e1caf62843be005ccc00bf54a21b308525d
onekingdev <onekingdev@gmail.com>
2021-11-24 14:14:14 -0600

    question fragments added

---
+++ src/api/fragments/questionFragments.ts
@@ -0,0 +1,91 @@
+
+export const UNIVERSAL_AOK = `
+    {
+        id
+        identifier
+        isActive
+        name
+        slug
+        randomSlug
+        deletedTimestamp
+        createTimestamp
+        updateTimestamp
+    }
+`;
+
+export const UNIVERSAL_TOPIC = `
+    {
+        id
+        identifier
+        isActive
+        randomSlug
+        name
+        slug
+        standardCode
+        lft
+        rght
+        treeId
+        level
+    }
+`;
+
+export const AREA_OF_KNOWLEDGE = `
+    {
+        id
+        identifier
+        randomSlug
+        hexColor
+        slug
+        image
+        name
+    }
+`;
+
+export const TOPIC = `
+    {
+        id
+        identifier
+        isActive
+        randomSlug
+        slug
+        lft
+        rght
+        treeId
+        level
+        name
+    }
+`;
+
+export const TOPIC_GRADE = `
+    {
+        id
+        identifier
+        isActive
+        randomSlug
+        standardCode
+    }
+`;
+
+export const QUESTION = `
+    {
+        id
+        identifier
+        isActive
+        randomSlug
+        questionText
+    }
+`;
+
+export const ANSWER_OPTION = `
+    {
+        id
+        identifier
+        randomSlug
+        isCorrect
+        answerText
+        explanation
+        image
+        audioFile
+        video
+    }
+`;
