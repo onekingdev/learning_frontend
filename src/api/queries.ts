cd32886784a1c6c3c9d23d122d50a5f695e9f2a2
onekingdev <onekingdev@gmail.com>
2021-11-26 10:53:08 -0600

    get function changed

--- src/api/queries.ts
+++ src/api/queries.ts
@@ -1,17 +1,21 @@
 export const get = (
+  queryName: string,
   query: string,
   handleData: (x: any) => void,
   handleError: (x: any) => void
 ) => {
-  return fetch('http://localhost:8000/graphql/', {
+  return fetch('http://143.244.183.24/graphql/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      query: query,
+      query: `{
+        ${queryName} ${query}
+      }`,
     }),
   })
+    .then(response => response.json())
     .then(handleData)
     .catch(handleError);
 };
