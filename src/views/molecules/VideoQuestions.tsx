7572306ae542812d9cd8094008e2bf0eb492bb2e
onekingdev <onekingdev@gmail.com>
2022-03-23 20:04:34 +0100

    change video of question box

--- src/views/molecules/VideoQuestions.tsx
+++ src/views/molecules/VideoQuestions.tsx
@@ -7,7 +7,9 @@ type VideoProps = {
 export const VideoQuestion = ({source}: VideoProps) => {
   return (
     <>
-      <VideoStyles src={source} />
+      {/* <iframe href="//www.youtube.com/watch?v=xbBr5b3-wSQ" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"width="400" height="230" src="https://youtube.com/embed/XgT618-arN8"></iframe> */}
+      <VideoStyles src={source} allowFullScreen allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"/>
+      {/* <iframe src={source}/> */}
     </>
   );
 };
