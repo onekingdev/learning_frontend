0979364f809f7015b3ce0550d25aa7b1c53d7dad
onekingdev <onekingdev@gmail.com>
2022-04-05 02:37:12 +0200

    merged with kifune

--- src/views/pages/Parent/Payment/dictionary.ts
+++ src/views/pages/Parent/Payment/dictionary.ts
@@ -1,19 +1,13 @@
 export const dictionary = Object.freeze({
     "en-us": {
-        addingANewChild : "Adding a New Child",
-        pleaseCompleteTheFollowingToGetYourChildStarted : "Please complete the following to get your child started",
-        selectYourPackage: " Select Your Package",
-        childFirstName: "Child First Name",
-        firstLetterofChildLastName: "First Letter of Child Last Name",
-        childUsernameDoNotUseYourRealName: "Child Username (DO NOT USE YOUR REAL NAME)",
-        password: "Password",
-        confirmPassword: "Confirm Password",
-        selectYourCurriculum: "Select Your Curriculum",
-        selectYourGrade: "Select Your Grade",
-        previousKid: "Previous Kid",
-        nextKid: "Next Kid",
-        finish: "Finish",
-        fieldIsRequired: 'Field is required',
+        choosePackageInfo: `When signing up for Socrates, children can get one or more areas of knowledge to learn from our growing selection.Choose the Solo package for one area, Combo package for any two, or Gold Package to get it all!`,
+        math: 'Math',
+        elaSgithWords: "ELA + SIGHT WORDS",
+        science: "SCIENCE",
+        financialLiteracy: "FINANCIAL LITERACY",
+        healthAndSafety: "HEALTH & SAFETY",
+        receive: "Receive",
+        offThesePricesForEachAdditionalChildOnYourAccount: "off these prices for each additional child on your account"
     },
     "th": {
         addingANewChild : "การเพิ่มเด็กใหม่",
