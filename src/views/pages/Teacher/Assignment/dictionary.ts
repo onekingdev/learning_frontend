1b99dc0eb211f0a9b299d1f29e6803115d6ff603
onekingdev <onekingdev@gmail.com>
2022-04-25 23:28:58 +0900

    am going to move results page

--- src/views/pages/Teacher/Assignment/dictionary.ts
+++ src/views/pages/Teacher/Assignment/dictionary.ts
@@ -1,11 +1,14 @@
 export const dictionary: any = Object.freeze({
     'EN_US': {
         title: 'Assignment',
+        assign: 'Assign'
     },
     'TH' :{
         title: 'Assignment',
+        assign: 'Assign'
     },
     'ES_MX': {
         title: 'Assignment',
+        assign: 'Assign'
     }
 });
