793748d54ed4551bbd9be60ef664f0694937f5e4
onekingdev <onekingdev@gmail.com>
2022-04-30 05:04:37 +0900

    working on teacher progress student page and need account info

---
+++ src/views/pages/Teacher/ProgressStudent/dictionary.ts
@@ -0,0 +1,14 @@
+export const dictionary: any = Object.freeze({
+    'EN_US': {
+        correct_answer: 'Correct Answer',
+        coins_earned: 'Coins earned'
+    },
+    'TH' :{
+        correct_answer: 'Correct Answer',
+        coins_earned: 'Coins earned'
+    },
+    'ES_MX': {
+        correct_answer: 'Correct Answer',
+        coins_earned: 'Coins earned'
+    }
+});
