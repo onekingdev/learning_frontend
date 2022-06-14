922d11b589ac26d2d43ca1bd1bbe699159fc835f
onekingdev <onekingdev@gmail.com>
2022-06-15 07:50:26 +0900

    complete 404 page

--- src/views/pages/Welcome/Error404.tsx
+++ src/views/pages/Welcome/Error404.tsx
@@ -3,20 +3,20 @@ import { useHistory } from 'react-router-dom';
 import { resetReducer } from 'app/actions/userActions';
 import { useDispatch, useSelector } from 'react-redux';
 import {
+  Box,
+  Button,
   ThemeProvider,
   useMediaQuery
 } from '@mui/material';
-import { welcomePage } from 'views/Theme';
-
-import {
-  Wrapper,
-} from './Style';
+import { themeTeacher } from 'views/Theme';
+import notFoundPageImage from 'views/assets/others/404image.svg'
 import { Typography } from '@mui/material';
 import { ScreenSize } from 'constants/screenSize';
+import { Wrapper } from './Style';
 
 export const Error404: FC = () => {
   const isMobile = useMediaQuery(`(max-width: ${ScreenSize.phone})`)
-  const [deployModal] = useState(false);
+
   const history = useHistory();
   const dispatch = useDispatch();
   let language: string = useSelector((state: any) => state.user.language);
@@ -27,10 +27,23 @@ export const Error404: FC = () => {
   }, []);
 
   return (
-    <ThemeProvider theme={welcomePage}>
+    <ThemeProvider theme={themeTeacher}>
       <Wrapper>
-        <Typography variant='h3'>Error: 404, Page not found!</Typography>
+        <Box display='flex' flexDirection='column' alignItems='center' gap={3}>
+          <Typography variant='h1' fontWeight={'bold'}>Opps!</Typography>
+          <Typography variant='h4'>Something went wrong.</Typography>
+          <Typography variant='h5'>Error404: Page not found</Typography>
+          <img src={notFoundPageImage}
+            style={{ maxWidth: '100%', maxHeight: '60vh' }}
+          />
+          <Button
+            variant='contained'
+            color='aqua'
+            onClick={() => history.goBack()}
+            sx={{ marginBottom: 10 }}
+          >Go Back</Button>
+        </Box>
       </Wrapper>
-    </ThemeProvider>
+    </ThemeProvider >
   );
 };
